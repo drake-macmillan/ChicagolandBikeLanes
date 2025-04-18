0f027ea4c78aba4c2ee07206664c5be0b78b7961
Drake MacMillan <123581664+drake-macmillan@users.noreply.github.com>
2025-04-18 12:29:31 -0500

    Update style-rules.js

--- style-rules.js
+++ style-rules.js
@@ -4,10 +4,10 @@ function getStyle(feature) {
 
   const styles = {
     "Protected Bike Lane":        { color: "#2547cf", weight: 3, dashArray: null },
-    "Painted Lane, Large":        { color: "#e05b1d", weight: 3, dashArray: "4,4" },
-    "Painted Lane":               { color: "#e05b1d", weight: 1, dashArray: "4,4" },
-    "2-way Mellow Route/Greenway":{ color: "#44a832", weight: 3, dashArray: "5,10" },
-    "1-way Mellow Route/Greenway":{ color: "#44a832", weight: 2, dashArray: "5,10" },
+    "Painted Lane, Large":        { color: "#e05b1d", weight: 3, dashArray: "2,4" },
+    "Painted Lane":               { color: "#e05b1d", weight: 1, dashArray: "2,4" },
+    "2-way Mellow Route/Greenway":{ color: "#2547cf", weight: 2, dashArray: "4,4" },
+    "1-way Mellow Route/Greenway":{ color: "#2547cf", weight: 1, dashArray: "4,4" },
     "Off-Street Trail/Path":      { color: "#2547cf", weight: 4, dashArray: null },
   };
 
