f9ad0aea41a62b3eaf194c845c9e4d749bf8b97f
Drake MacMillan <123581664+drake-macmillan@users.noreply.github.com>
2025-04-18 12:13:33 -0500

    Update style-rules.js

--- style-rules.js
+++ style-rules.js
@@ -3,12 +3,12 @@ function getStyle(feature) {
   const type = feature.properties.displayrou;
 
   const styles = {
-    "Protected Bike Lane":        { color: "#2547cf", weight: 4, dashArray: null },
-    "Painted Lane, Large":        { color: "#c9ba2c", weight: 3 },
-    "Painted Lane":               { color: "#e05b1d", weight: 2, dashArray: "4,4" },
-    "2-way Mellow Route/Greenway":{ color: "#44a832", weight: 3, dashArray: "1,6" },
-    "1-way Mellow Route/Greenway":{ color: "#44a832", weight: 2, dashArray: "1,6" },
-    "Off-Street Trail/Path":      { color: "#44a832", weight: 4, dashArray: "1,6" },
+    "Protected Bike Lane":        { color: "#2547cf", weight: 3, dashArray: null },
+    "Painted Lane, Large":        { color: "#e05b1d", weight: 3, dashArray: "4,4" },
+    "Painted Lane":               { color: "#e05b1d", weight: 1, dashArray: "4,4" },
+    "2-way Mellow Route/Greenway":{ color: "#44a832", weight: 3, dashArray: "5,10" },
+    "1-way Mellow Route/Greenway":{ color: "#44a832", weight: 2, dashArray: "5,10" },
+    "Off-Street Trail/Path":      { color: "#2547cf", weight: 4, dashArray: null },
   };
 
   return styles[type] || { color: "#AAAAAA", weight: 1 };
