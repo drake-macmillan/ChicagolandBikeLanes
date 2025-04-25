46a183426b74746a2b557c13bc3aa258ec613045
Drake MacMillan <123581664+drake-macmillan@users.noreply.github.com>
2025-04-25 11:29:01 -0500

    Update init-map.js to remove zoom buttons
    zoom buttons are unnecessary because user can pinch on mobile or use scroll wheel on desktop. needed to remove them in order to make room for other controls

--- js/init-map.js
+++ js/init-map.js
@@ -1,7 +1,7 @@
 // init-map.js
 
 // Initialize the Leaflet map and center it on Chicago
-const map = L.map('map').setView([41.8781, -87.6298], 13);
+const map = L.map('map', { zoomControl: false }).setView([41.8781, -87.6298], 13);
 
 // Add OpenStreetMap tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
