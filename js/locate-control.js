653b9a6e3173393609c4b4547a1ff0102eba4794
Drake MacMillan <123581664+drake-macmillan@users.noreply.github.com>
2025-04-25 11:23:33 -0500

    Update locate-control.js
    removed some code related to prompting user to allow location. I don't think it was necessary and can always add back later

--- js/locate-control.js
+++ js/locate-control.js
@@ -10,21 +10,3 @@ const locateControl = L.control.locate({
     enableHighAccuracy: true
   }
 }).addTo(map);
-
-// Trigger location request when searching
-geocoder.on('markgeocode', function (e) {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      locateControl.start();
-    }, function (err) {
-      console.error("Geolocation error: " + err.message);
-    });
-  }
-});
-
-// Optional: Prevent re-prompting if already granted
-if (localStorage.getItem('location-granted') !== 'true') {
-  locateControl.start();
-  localStorage.setItem('location-granted', 'true');
-}
-
