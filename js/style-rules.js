80d0b3e592ba334b8c5dd57ad2418bfa7f97a365
Drake MacMillan <123581664+drake-macmillan@users.noreply.github.com>
2025-05-30 09:29:58 -0500

    adjusting line thiccness

--- js/style-rules.js
+++ js/style-rules.js
@@ -4,11 +4,11 @@ function getStyle(feature) {
 
   const styles = {
     "Protected Bike Lane":        { color: "#2547cf", weight: 4, dashArray: null },
-    "Painted Lane, Large":        { color: "#e05b1d", weight: 4, dashArray: "4,8" },
+    "Painted Lane, Large":        { color: "#e05b1d", weight: 3.5, dashArray: "4,8" },
     "Painted Lane":               { color: "#e05b1d", weight: 1.5, dashArray: "4,8" },
     "2-way Mellow Route/Greenway":{ color: "#2547cf", weight: 3, dashArray: "4,4" },
     "1-way Mellow Route/Greenway":{ color: "#2547cf", weight: 2, dashArray: "4,4" },
-    "Off-Street Trail/Path":      { color: "#000080", weight: 5, dashArray: null },
+    "Off-Street Trail/Path":      { color: "#000080", weight: 4.5, dashArray: null },
   };
 
   return styles[type] || { color: "#AAAAAA", weight: 1 };
