b030bfa62292eca8fa383639dd99ba67191beab5
Drake MacMillan <123581664+drake-macmillan@users.noreply.github.com>
2025-06-02 13:56:03 -0500

    loading bike lanes via main.js instead of script in index.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+function loadBikeLanes() {
+  fetch('data/chicagoland_bikeways_10may25.geojson')
+    .then(response => response.json())
+    .then(data => {
+      L.geoJSON(data, {
+        style: getStyle,
+      }).addTo(map); // map comes from init-map.js
+    });
+}
 
+loadBikeLanes();
