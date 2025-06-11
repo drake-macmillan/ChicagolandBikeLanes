14c1fa4e9c42985c8e3648826c2a0219902a12ef
Drake MacMillan <123581664+drake-macmillan@users.noreply.github.com>
2025-06-11 15:30:12 -0500

    added onClick event to view feature metadata in console

--- js/main.js
+++ js/main.js
@@ -3,12 +3,29 @@ import { addMapControls } from './map-controls.js';
 import { getStyle } from './style-rules.js';
 import { loadDivvyStations } from './divvyLayer.js';
 
+/*
 function loadBikeLanes() {
   fetch('data/chicagoland_bikeways_jun10.geojson')
     .then(response => response.json())
     .then(data => {
       L.geoJSON(data, {
         style: getStyle,
+      }).addTo(map);
+    });
+}
+*/
+
+function loadBikeLanes() {
+  fetch('data/chicagoland_bikeways_jun10.geojson')
+    .then(response => response.json())
+    .then(data => {
+      L.geoJSON(data, {
+        style: getStyle,
+        onEachFeature: function(feature, layer) {
+          layer.on('click', function(e) {
+            console.log('Feature clicked:', feature.properties);
+          });
+        }
       }).addTo(map);
     });
 }
