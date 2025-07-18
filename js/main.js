3205f75ade01cf22646d84fda335cbb24cc16305
Drake MacMillan <123581664+drake-macmillan@users.noreply.github.com>
2025-07-18 16:26:03 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import { getStyle } from './style-rules.js';
 import { loadDivvyStations } from './divvyLayer.js';
 
 function loadBikeLanes() {
-  fetch('data/chicagoland_bikeways_july18pm.json')
+  fetch('data/chicagoland_bikeways_july19.json')
     .then(response => response.json())
     .then(data => {
       L.geoJSON(data, {
