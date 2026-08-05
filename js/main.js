3e201b9e13e72f208bab0f946add15a833fe7f3c
Drake MacMillan <123581664+drake-macmillan@users.noreply.github.com>
2026-08-05 15:18:06 -0500

    humboldt park and clark updates

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import { getStyle } from './style-rules.js';
 import { loadDivvyStations } from './divvyLayer.js';
 
 function loadBikeLanes() {
-  fetch('data/chicagoland_bikeways_june11pm.json')
+  fetch('data/chicagoland_bikeways_august5.json')
     .then(response => response.json())
     .then(data => {
       L.geoJSON(data, {
